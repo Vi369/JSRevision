8c2296988007aab30a4517c006d5fced5dcdc8ca
vi369 <vikashverma3001@gmai.com>
2023-10-23 18:08:21 +0530

    conversation of data types

---
+++ conversation.js
@@ -0,0 +1,34 @@
+let score = "33abc"
+
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//always cross check beacuse in js Nan also return the type number
+
+
+//"33" => 33
+//"33abc" => NaN
+
+// true => 1 false => 0
+
+
+
+let isLoggedIn = 1;
+
+let boolValue = Boolean(isLoggedIn);
+
+// console.log(boolValue);
+
+
+// empty string => false
+
+//  "hello" => true
+
+let someNumber =33
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
