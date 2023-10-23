a1f781270b9a641025a3571cd56d495b0e480e88
vi369 <vikashverma3001@gmai.com>
2023-10-23 17:51:15 +0530

    data types

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as newer version of code
+
+// alert(3+3) // we are using node js n not browser
+
+console.log(3+
+    3) // read usability keep always high and clean
+
+    // number 
+    // bigint
+    // string
+    // boolean => true false
+    // null => standalone value
+    // undefined => not assing value
+    // symbol => for unique ness 
+
+
+    // object
+    
+    console.log(typeof null)
+    console.log(typeof undefined)
